10ee3d3c79298aa4d67085c822039051dc74e3bb
Plinio Santana <pliniosb@gmail.com>
2022-05-04 01:07:57 -0300

    Incluindo visões para location (info e review)

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,15 @@
+const dotenv = require('dotenv').config()
+const env = dotenv.parsed
+
 module.exports.homeList = function(req, res){
     res.render('locations-list', { title: 'Home' });
 };
 
 module.exports.locationInfo = function(req, res){
-    res.render('index', { title: 'Location Info' });
+    let googleApiKey = env.GOOGLE_API_KEY
+    res.render('location-info', { title: 'Location Info', apiKey: googleApiKey });
 };
 
 module.exports.addReview = function(req, res){
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add review' });
 };
