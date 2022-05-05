a8e5d611c79301a491e0aaf81920d6b71daa3ee1
Plinio Santana <pliniosb@gmail.com>
2022-05-04 23:15:05 -0300

    Visões inteligentes usando dados do controlador

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,7 +2,34 @@ const dotenv = require('dotenv').config()
 const env = dotenv.parsed
 
 module.exports.homeList = function(req, res){
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', {
+      title: 'Loc8r - find a place to work with wifi',
+      pageHeader: {
+        title: 'Loc8r',
+        strapline: 'Find places to work wifi wifi near you!'
+      },
+      locations: [{
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+      },
+      {
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '200m'
+      },
+      {
+        name: 'Pizza Discovery',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 2,
+        facilities: ['Food', 'Premium wifi'],
+        distance: '250m'
+      }]
+    });
 };
 
 module.exports.locationInfo = function(req, res){
