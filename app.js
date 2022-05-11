05df096994622cff2f1125a4bdcfa807b8128871
Plinio Santana <pliniosb@gmail.com>
2022-05-10 23:34:13 -0300

    Criando as rotas

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./app_server/models/db');
 
-var indexRouter = require('./app_server/routes/index');
+var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index')
 var usersRouter = require('./app_server/routes/users');
 
 var app = express();
@@ -20,7 +21,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', routes);
+app.use('/api', routesApi);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
