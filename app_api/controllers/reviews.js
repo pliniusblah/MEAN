06d013d7f9cf43300d60ec17ea3120e7b4ab8650
Plinio Santana <pliniosb@gmail.com>
2022-05-15 19:25:09 -0300

    Requisições POST

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,13 +1,93 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
-var sendJSONResponse = function (res, status, content) {
+const sendJSONResponse = function (res, status, content) {
   res.status(status);
   res.json(content);
 };
 
+const doSetAverageRating = function(location){
+  let reviewCount, ratingAverage, ratingTotal;
+
+  if(location.reviews && location.reviews.length > 0){
+    reviewCount = location.reviews.length;
+    ratingTotal = 0;
+    for(let count = 0; count < reviewCount; count++){
+      ratingTotal = ratingTotal + location.reviews[i].rating;
+    }
+    ratingAverage = parseInt(ratingTotal / reviewCount, 10);
+    location.rating = ratingAverage;
+    location.save(function(err){
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log(`Average rating updated to ${ratingAverage}`);  
+      }
+    });
+  }
+};
+
+const updateAverageRating = function(locationid){
+  Loc
+    .findById(locationid)
+    .select('rating reviews')
+    .exec(
+      function(err, location){
+        if(!err){
+          doSetAverageRating(location);
+        }
+      });
+};
+
+const doAddReview = function(req, res, location){
+  if (!location) {
+    sendJSONResponse(res, 404, {
+      "message": "locationid not found"
+    });
+  }
+  else {
+    location.reviews.push({
+      author: req.body.author,
+      rating: req.body.rating,
+      reviewText: req.body.reviewText
+    });
+    location.save(function(err, location){
+      let thisReview;
+      if(err) {
+        sendJSONResponse(res, 400, err);
+      }
+      else {
+        updateAverageRating(location._id);
+        thisReview = location.reviews[location.reviews.length - 1];
+        sendJSONResponse(res, 201, thisReview);
+      }
+    });  
+  }
+};
+
 module.exports.reviewsCreate = function (req, res) {
-  sendJSONResponse(res, 200, { "status": "success " });
+  var locationid = req.params.locationid;
+
+  if (locationid) {
+    Loc
+      .findById(locationid)
+      .select('reviews')
+      .exec(
+        function(err, location) {
+          if (err) {
+            sendJSONResponse(res, 400, err);
+          } else {
+            doAddReview(req, res, location);
+          }
+        }
+      );
+  }
+  else {
+    sendJSONResponse(res, 404, {
+      "message": "Not found, locationid required"
+    });
+  }
 };
 
 module.exports.reviewsReadOne = function (req, res) {
